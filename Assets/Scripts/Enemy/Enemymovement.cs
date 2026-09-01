5180fd34b6a563ba39b0da07b6bb16fad34beae4
Divyanshu Khoiya <divyanshu98712@gmail.com>
2026-09-01 21:33:33 +0530

    feat: add enemy chasing and audio

--- Assets/Scripts/Enemy/Enemymovement.cs
+++ Assets/Scripts/Enemy/Enemymovement.cs
@@ -1,7 +1,13 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    AudioSource[] audiosources;
+    [SerializeField] float separationRadius = 1.0f;
+    [SerializeField] float separationStrength = 0.2f;
+    [SerializeField] LayerMask enemyLayer;
+    Vector2 separationDirection;
     [SerializeField] float moveSpeed = 2f;
     [SerializeField] float attackrange = 1.5f;
     [SerializeField] float attackcooldown = 1f;
@@ -12,6 +18,8 @@ public class EnemyMovement : MonoBehaviour
     playerhealth playerhealth;
     int damage = 25;
     Transform player;
+    Health health;
+    
 
     void Awake()
     {
@@ -19,11 +27,13 @@ public class EnemyMovement : MonoBehaviour
         playerhealth = player.GetComponent<playerhealth>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        health = GetComponent<Health>();
+        audiosources = GetComponents<AudioSource>();
     }
 
     void Update()
     {
-        if(player == null) return;
+        if(player == null || health.isdead) return;
 
         float distance = Vector2.Distance(player.position, transform.position);
         if (distance > attackrange)
@@ -37,15 +47,17 @@ public class EnemyMovement : MonoBehaviour
             if(Time.time > nextattacktime)
             {
                 animator.SetTrigger("Attack");
+                audiosources[0].Play();
                 playerhealth.takedamage(damage);
                 nextattacktime = Time.time + attackcooldown;
             }
         }
     }
+    
 
     void FixedUpdate()
     {
-        if(player == null) return;
+        if(player == null || health.isdead) return;
 
         if (shouldchase)
         {
@@ -58,7 +70,22 @@ public class EnemyMovement : MonoBehaviour
     {
         Vector2 direction = player.position - transform.position;
         direction.Normalize();
+        Collider2D[] nearbyEnemies =
+        Physics2D.OverlapCircleAll(transform.position, separationRadius, enemyLayer);
 
+        separationDirection = Vector2.zero;
+
+        foreach (Collider2D enemy in nearbyEnemies)
+        {
+            if (enemy.gameObject == gameObject)
+                continue;
+
+            Vector2 awayFromEnemy = (Vector2)transform.position - (Vector2)enemy.transform.position;
+            separationDirection += awayFromEnemy.normalized;
+        }
+
+        direction += separationDirection * separationStrength;
+        direction.Normalize();
         rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
     }
     
--- Assets/Scripts/Enemy/enemyhealth.cs
+++ Assets/Scripts/Enemy/enemyhealth.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    AudioSource[] audiosources;
     [SerializeField] int MaxHealth = 100;
     int CurrentHealth;
     Animator animator;
+    public bool isdead;
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        audiosources = GetComponents<AudioSource>();
     }
 
     void Start()
@@ -18,11 +21,18 @@ public class Health : MonoBehaviour
 
     public void EnemyTakeDamage(int damage)
     {
+        if(isdead) return;
         CurrentHealth -= damage;
         if(CurrentHealth <= 0)
         {
+            isdead = true;
             animator.SetTrigger("Death");
-            
+            audiosources[1].Play();
         }
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 }
